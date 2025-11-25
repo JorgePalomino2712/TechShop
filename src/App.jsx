6c8df067eb86882fb3a04a2d1057f04a43ae4ad0
Jorge2712 <jorgeps2712@gmail.com>
2025-11-25 15:42:22 -0500

    Add routing and login page with styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
+import Login from "./pages/Login"
+import Home from "./pages/Home"
+import { Routes, Route, Navigate } from 'react-router-dom'
 
 export default function App() {
   return (
-    <div>
-
-    </div>
+    <>
+      <Routes>
+        <Route path='/' element={<Login></Login>} />
+        <Route path='/home' element={<Home></Home>} />
+        <Route path='*' element={<Navigate to="/" />} />
+      </Routes>
+    </>
   )
 }
