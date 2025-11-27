7acaaa7c877c0dc930dcb69c0182d70e9db29171
ibraim <ing.gonzalezvij@hotmail.com>
2025-11-27 08:36:06 -0600

    landing page agregada

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,15 +1,16 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
-
+import PublicHeader from '../components/headers/PublicHeader'
+import Footer from '../components/footers/Footer'
 
 export default function MainLayout() {
     return (
         <div className="min-h-screen flex flex-col">
-
+            <PublicHeader/>
             <main className="flex-1">
                 <Outlet />
             </main>
-
+            <Footer/>
         </div>
     )
 }
