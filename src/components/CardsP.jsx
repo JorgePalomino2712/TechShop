a1ead4e98d06e802985b558b541c17c92e1abd6f
Jorge2712 <jorgeps2712@gmail.com>
2025-11-27 09:34:37 -0500

    agregando cards

---
+++ src/components/CardsP.jsx
@@ -0,0 +1,66 @@
+import React from 'react'
+import { useState, useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
+
+export default function CardsP() {
+    const [error, setError] = useState(null)
+    const [loading, setLoading] = useState(true)
+    const [product, setProducts] = useState([])
+    const navigate = useNavigate();
+
+
+    useEffect(() => {
+        async function traerCards() {
+            try {
+                const respuesta = await fetch(
+                    "https://api-funval-g6.onrender.com/products/?skip=0&limit=100",
+                    {
+                        method: "GET",
+                        headers: {
+                            "accept": "application/json",
+                        },
+
+                    }
+                );
+                const data = await respuesta.json();
+                console.log(data);
+                const filtrados = data.filter(item => item.category === "audio");
+                setProducts(filtrados);
+
+
+            } catch (err) {
+                setError(err.message);
+            }
+        }
+
+        traerCards()
+    }, []);
+    return (
+        <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-2 p-5 items-center justify-center'>
+            {error && <p className="text-red-400 text-sm text-center mt-2">{error}</p>}
+            {product.map((p) => (
+                <div key={p.id} className="w-60 h-80 bg-gray-50 p-3 flex flex-col gap-1 rounded-2xl ">
+                    <img src={p.image_url} className='h-48 bg-gray-700 rounded-xl' alt="" />
+                    <div className="flex flex-col gap-4">
+                        <div className="flex flex-row justify-between">
+                            <div className="flex flex-col">
+                                <span className="text-xl font-bold">Long Chair</span>
+                                <p className="text-xs text-gray-700">ID: 23432252</p>
+                            </div>
+                            <span className="font-bold  text-red-600">$25.99</span>
+                        </div>
+                        <button onClick={() => {
+                            navigate("/")
+                        }} className="hover:bg-sky-700 text-gray-50 bg-sky-800 py-2 rounded-md">
+                            Add to cart
+                        </button>
+                    </div>
+                </div>
+
+
+            ))}
+
+
+        </div>
+    )
+}
